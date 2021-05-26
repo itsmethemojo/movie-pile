cf59f6aa02a44503a5b00857c54a8f69bbe3f99e
Marian Poeschmann <github@mail.itsmethemojo.eu>
2021-05-27 01:53:12 +0200

    extract complex javascript from html, add format and linting build steps for javascript

--- views/javascript/index.js
+++ views/javascript/index.js
@@ -1,36 +1,41 @@
-function init(){
+/*jshint esversion: 6 */
+
+function init() {
   try {
-    movie_piles_data = JSON.parse(window.localStorage.getItem("movie_piles_data"));
+    moviePileData = JSON.parse(window.localStorage.getItem('movie_piles_data'));
   }
   finally {
-    if (movie_piles_data === null){
-      movie_piles_data = {};
+    if (moviePileData === null) {
+      moviePileData = {};
     }
   }
-  if(Object.entries(movie_piles_data).length > 0){
+  if (Object.entries(moviePileData).length > 0) {
     html = '<h2> your piles</h2>';
-    Object.keys(movie_piles_data).forEach(movie_pile_url => {
-       html += '<p><a href="' + movie_pile_url + '">' + movie_piles_data[movie_pile_url].name + '</a></p>'
+    Object.keys(moviePileData).forEach(moviePileUrl => {
+      html += '<p><a href="' +
+        moviePileUrl + '">' +
+        moviePileData[moviePileUrl].name +
+        '</a></p>';
     });
-    document.getElementById("existing-movie-piles").innerHTML = html;
+    document.getElementById('existing-movie-piles').innerHTML = html;
   }
 }
 
 init();
 
-function start(){
+function start() {
   var request = new XMLHttpRequest();
-  request.open( "POST", "/api/v2/movie-pile/create", false );
+  request.open('POST', '/api/v2/movie-pile/create', false);
   request.setRequestHeader('Content-Type', 'application/json');
   request.setRequestHeader('Accept', 'application/json');
   request.send(
     JSON.stringify(
       {
-        name: "Movie Pile " + new Date().toISOString().slice(0, 10),
-        movie_list: []
+        'name': 'Movie Pile ' + new Date().toISOString().slice(0, 10),
+        'movie_list': []
       }
     )
   );
-  var movie_pile = JSON.parse(request.responseText);
-  window.location.href= '/v2/' + movie_pile['id'] + '/edit/' + movie_pile['secret'];
+  var moviePile = JSON.parse(request.responseText);
+  window.location.href = '/v2/' + moviePile.id + '/edit/' + moviePile.secret;
 }
