aeff922930c6668575160dfcf7fc5e39cef3785c
Marian Poeschmann <github@mail.itsmethemojo.eu>
2026-09-10 00:40:26 +0200

    feat: allows movie urls to be saved if meta data can not be fetched

--- src/services/parallel_request_service.rb
+++ src/services/parallel_request_service.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'typhoeus'
+require 'logger'
 
 # service to run multiple parallel http requests
 class ParallelRequestService
@@ -18,6 +19,8 @@ class ParallelRequestService
     hydra = Typhoeus::Hydra.new
     requests = []
     urls.each do |url|
+      # TODO insert user agent from incoming request?
+      # headers: {"User-Agent": useragent}
       request = Typhoeus::Request.new(url, followlocation: true)
       hydra.queue(request)
       requests.push(request)
@@ -28,9 +31,11 @@ class ParallelRequestService
 
   def get_responses(requests, expand: false)
     responses = []
+    logger = Logger.new(STDOUT)
     requests.each do |request|
       response_body = request.response.body
       response_body = '' if request.response.code != 200
+      logger.error("unexpected response code " + request.response.code.to_s + " for url " + request.url)
       response = expand ? [request.url, response_body] : response_body
       responses.push(response)
     end
