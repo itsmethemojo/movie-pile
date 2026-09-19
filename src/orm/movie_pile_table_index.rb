ca92cf9462dd9c2d719172e8e19d692af634a74b
Marian Poeschmann <github@mail.itsmethemojo.eu>
2026-09-19 21:38:25 +0200

    fix: do not break migration if index exists

--- src/orm/movie_pile_table_index.rb
+++ src/orm/movie_pile_table_index.rb
@@ -5,7 +5,7 @@ require 'sinatra/activerecord'
 # sets up the missing index on pile_id
 class MoviePileTableIndex < ActiveRecord::Migration[5.2]
   def up
-    add_index :movie_piles, :pile_id
+    add_index :movie_piles, :pile_id, unique: true, if_not_exists: true
   end
 
   def down
